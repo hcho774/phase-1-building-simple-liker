a4f796d9a1c3a1b0e3ee8ba288fe593b9381fb16
Hyun Sung Cho <hcho774@gmail.com>
2022-04-13 06:04:57 -0400

    commit tree e17b6e3a51851b3bd618c1fe06ca4f71c636378b

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// going to grab error! and add class attribute "hidden"
+const errorDiv = document.getElementById("modal");
+errorDiv.className = "hidden";
+//grab all empty heart which has class name of "like-glyph"
+
+let spans = document.querySelectorAll(".like-glyph");
+
+for (let i = 0; i < spans.length; i++) {
+  spans[i].addEventListener("click", function(e){
+    let heart = e.target;
+    mimicServerCall()
+    .then(function(data){
+      if(heart.innerText === EMPTY_HEART){
+        heart.setAttribute("class", "activated-heart");
+        heart.innerText = FULL_HEART;
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      };
+    })
+    .catch(function(error){
+      errorDiv.className = "";
+      errorDiv.innerText = error;
+      setTimeout(() => errorDiv.className = "hidden", 3000);
+    })
+  })
+}
+
+
+
+//add click event to like button
 
 
 
